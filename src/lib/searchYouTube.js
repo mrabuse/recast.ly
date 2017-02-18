aaae362ffa935e6b34fa4fadf324093b846e60b2
mrabuse <cogglemat@gmail.com>
2017-02-18 12:30:24 -0600

    Complete searchYouTube specs

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,26 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    method: 'GET',
+    data: {
+      part: 'snippet',
+      q: options.query,
+      order: 'relevance',
+      maxResults: options.max || 5,
+      videoEmbeddable: true,
+      type: 'video',
+      key: options.key
+    },
+    cache: false,
+    success: function(data) {
+      callback(data.items);
+    },
+    error: function(xhr) {
+      console.log('Sad day no work');
+    }
+  });
 };
+
 
 window.searchYouTube = searchYouTube;
